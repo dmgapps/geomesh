5e5915b79dcecf8be4e4e4909de6f371e5fd4680
Digital Marketing Group <brandon@digitalmktgrp.com>
2014-06-16 23:30:27 -0400

    update

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ $(document).on( "mobileinit", function() {
 });
 
 
+/*
 function uploadVideo() {
 	console.log('upload video');
-
 	var options = {
 
 				quality: 100,
@@ -45,4 +45,54 @@ function success(fileURI) {
 
 function error(message) {
     alert(message);
+}
+
+*/
+
+function uploadVideo() {
+
+	var options = {
+	                    quality : 100,
+	                    destinationType : Camera.DestinationType.FILE_URI,
+	                    sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+	                    mediaType: Camera.MediaType.VIDEO
+	                    targetWidth: 500,
+	                    targetHeight: 500,
+	            };
+
+
+	navigator.camera.getPicture(success, fail, options);
+
+}
+
+
+function success(fileURI) {
+
+	     
+	        var options = new FileUploadOptions();
+	        options.fileKey = "file";
+	        options.fileName = fileURI.substr(fileURI.lastIndexOf('/') + 1);
+	        options.mimeType = "application/octet-stream";
+	        var ft = new FileTransfer();
+	        ft.upload(fileURI, encodeURI('http://dmgdemos.com/geomesh/upload.php'), uploadSuccess, uploadFail, options);
+	}
+
+function fail(message) {
+	    console.log(message);
+}
+
+
+
+/* Upload callbacks */
+
+function uploadSuccess(data) {
+
+	console.log(data);
+
+}
+
+function uploadFail(data) {
+
+	console.log(data);
+
 }
